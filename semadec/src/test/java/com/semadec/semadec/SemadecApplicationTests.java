77836ab12329221d86a0293175dde0cc3dc124a3
Família <Famlia@DESKTOP-M9AHKL0>
2019-11-29 12:26:24 -0200

    Clonando o projeto.

---
+++ semadec/src/test/java/com/semadec/semadec/SemadecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semadec.semadec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemadecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
